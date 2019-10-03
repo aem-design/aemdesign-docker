4d5a4a95a6b939762b3884f7eb9497fb2ef4e140
wildone <max.barrass@gmail.com>
2019-10-03 23:16:08 +1000

    remove param to allow source

--- scripts/travis/get_config.sh
+++ scripts/travis/get_config.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
 
-DOCKER_FILE=${1?Need docker file as source}
-
 echo ">>> GET CONFIG FROM DOCKERFILE <<<"
-export IMAGE_NAME=$(grep imagename= "${DOCKER_FILE}" | sed -e 's/.*imagename="\(.*\)".*/\1/')
-export TEST_COMMAND=$(grep test.command= "${DOCKER_FILE}" | sed -e 's/.*test.command="\(.*\)".*/\1/')
-export TEST_COMMAND_VERIFY=$(grep test.command.verify= "${DOCKER_FILE}" | sed -e 's/.*test.command.verify="\(.*\)".*/\1/')
-export IMAGE_VERSION=$(grep version= "${DOCKER_FILE}" | sed -e 's/.*version="\(.*\)".*/\1/')
+export IMAGE_NAME=$(grep imagename= Dockerfile | sed -e 's/.*imagename="\(.*\)".*/\1/')
+export TEST_COMMAND=$(grep test.command= Dockerfile | sed -e 's/.*test.command="\(.*\)".*/\1/')
+export TEST_COMMAND_VERIFY=$(grep test.command.verify= Dockerfile | sed -e 's/.*test.command.verify="\(.*\)".*/\1/')
+export IMAGE_VERSION=$(grep version= Dockerfile | sed -e 's/.*version="\(.*\)".*/\1/')
 export IMAGE="${ORGANISATION_NAME}/${IMAGE_NAME}"
 echo "IMAGE_NAME=${IMAGE_NAME}"
 echo "IMAGE_VERSION=${IMAGE_VERSION}"
