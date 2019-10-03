ffde41a9e74086d89706a54eec626350e0e7f731
wildone <max.barrass@gmail.com>
2019-10-04 01:24:00 +1000

    use files to capture output of container test

--- scripts/travis/container_verify.sh
+++ scripts/travis/container_verify.sh
@@ -2,14 +2,23 @@
 
 # requires that get_config.sh has been executed.
 
-if [[ -z "${IMAGE}" ]] || [[ -z "${IMAGE_VERSION}" ]] || [[ -z "${TEST_COMMAND}" ]]; then
+if [[ -z "${IMAGE}" ]] || [[ -z "${IMAGE_VERSION}" ]] || [[ -z "${TEST_COMMAND}" ]] || [[ -z "${TEST_COMMAND_VERIFY}" ]]; then
     echo "please run get_config.sh"
     travis_terminate 1
 fi
 
 echo ">>> VERIFY BUILD CONTAINER <<<"
-export CONTAINER_COMMAND="docker run ${IMAGE}:${IMAGE_VERSION} ${TEST_COMMAND}"
-echo CONTAINER_COMMAND="${CONTAINER_COMMAND}"
-export CONTAINER_OUTPUT="$(eval ${CONTAINER_COMMAND})"
+DIR=$(mktemp -d)
+echo ${TEST_COMMAND}>"${DIR}/verify.bash"
+chmod +x "${DIR}/verify.bash"
+echo "TEST:docker run -v ${DIR}:/verify ${IMAGE}:${IMAGE_VERSION} ./verify/verify.bash>/verify/verify.output"
+docker run -v ${DIR}:/verify ${IMAGE}:${IMAGE_VERSION} ./verify/verify.bash>/verify/verify.output
+CONTAINER_OUTPUT=$(cat "${DIR}/verify.output")
 echo "CONTAINER_OUTPUT=${CONTAINER_OUTPUT}"
-if [[ ! ${CONTAINER_OUTPUT} =~ ${TEST_COMMAND_VERIFY} ]]; then travis_terminate 1; fi
+
+if [[ ! ${CONTAINER_OUTPUT} =~ ${TEST_COMMAND_VERIFY} ]]; then
+    echo ">>> TEST FAILED <<<"
+    echo "Expected: ${TEST_COMMAND_VERIFY}"
+    echo "Got: ${CONTAINER_OUTPUT}"
+    travis_terminate 1;
+fi
