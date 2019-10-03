7abb6887a0315f6aac0bf1441a6a29709a373ccb
wildone <max.barrass@gmail.com>
2019-10-03 22:42:40 +1000

    add error handling

--- scripts/travis/container_verify.sh
+++ scripts/travis/container_verify.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+# requires that get_config.sh has been executed.
+
+if [[ -z "${IMAGE}" ]] || [[ -z "${IMAGE_VERSION}" ]] || [[ -z "${TEST_COMMAND}" ]]; then
+    echo "please run get_config.sh"
+    travis_terminate 1
+fi
+
 echo ">>> VERIFY BUILD CONTAINE <<<"
 export CONTAINER_OUTPUT=$(docker run "${IMAGE}:${IMAGE_VERSION}" ${TEST_COMMAND})
 echo "CONTAINER_OUTPUT=${CONTAINER_OUTPUT}"
