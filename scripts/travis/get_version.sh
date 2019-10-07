76e4af6b164093d16524a537ca88b03135e7f3c3
wildone <max.barrass@gmail.com>
2019-10-07 17:50:30 +1100

    add error handling for build number

--- scripts/travis/get_version.sh
+++ scripts/travis/get_version.sh
@@ -2,6 +2,7 @@
 
 echo ">>> GET CONFIG FROM GIT <<<"
 export CURRENT_VERSION=$(git describe --tag --always --long | sed -e 's/\(.*\)-\(.*\)-.*/\1.\2/')
+echo "CURRENT_VERSION:${CURRENT_VERSION}"
 declare -a CURRENT_VERSION_ARRAY="(${CURRENT_VERSION//./ })";
 export SEMVER_MAJOR=${CURRENT_VERSION_ARRAY[0]};
 export SEMVER_MINOR=${CURRENT_VERSION_ARRAY[1]};
@@ -11,10 +12,18 @@ export SEMVER_BUILD=$(( ${SEMVER_PATCH} + ${SEMVER_BUILD} ))
 echo "SEMVER_MAJOR:${SEMVER_MAJOR}"
 echo "SEMVER_MINOR:${SEMVER_MINOR}"
 echo "SEMVER_BUILD:${SEMVER_BUILD}"
+
+REGEX_NUMBER='^[0-9]+$'
+if ! [[ $SEMVER_BUILD =~ $REGEX_NUMBER ]] ; then
+    echo "SEMVER_BUILD is not number RESET to 0"
+   SEMVER_BUILD=0
+fi
+
 export SEMVER=${SEMVER_MAJOR}.${SEMVER_MINOR}.${SEMVER_BUILD}
 echo "SEMVER:${SEMVER}"
-if [[ ${SEMVER_MAJOR} == "" ]];
-    then travis_terminate 1;
+if [[ ${SEMVER_MAJOR} == "" ]];then
+    echo PLEASE ADD TAG TO YOUR BRANCH
+    travis_terminate 1;
 fi
 export TRAVIS_TAG=${SEMVER}
 echo "TRAVIS_TAG:${TRAVIS_TAG}"
