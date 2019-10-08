bd6c42b49519b02aef5b038e9511a6dd0760e765
wildone <max.barrass@gmail.com>
2019-10-08 12:02:49 +1100

    let travis manage git clone

--- scripts/travis/get_version.sh
+++ scripts/travis/get_version.sh
@@ -3,9 +3,6 @@
 # Gets current version from Git log, need to have at least one tag for this to work
 
 echo ">>> GET CONFIG FROM GIT <<<"
-echo GET FULL GIT HISTORY
-git fetch --unshallow --tags
-echo $(git describe --tag --always --long)
 export CURRENT_VERSION=$(git describe --tag --always --long | sed -e 's/\(.*\)-\(.*\)-.*/\1.\2/')
 echo "CURRENT_VERSION:${CURRENT_VERSION}"
 declare -a CURRENT_VERSION_ARRAY="(${CURRENT_VERSION//./ })";
