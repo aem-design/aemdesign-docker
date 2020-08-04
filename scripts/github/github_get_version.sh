0cd4998b44ecf5d79d1e0afb15f15e9421cf7649
Max Barrass <max.barrass@gmail.com>
2020-08-04 22:36:17 +1000

    ignore unshallow errors

--- scripts/github/github_get_version.sh
+++ scripts/github/github_get_version.sh
@@ -5,7 +5,7 @@
 
 echo ">>> GET CONFIG FROM GIT <<<"
 echo GET FULL GIT HISTORY
-git fetch --unshallow --tags
+git fetch --unshallow --tags 2>&1 || true
 export CURRENT_VERSION=$(git describe --tag --always --long | sed -e 's/\(.*\)-\(.*\)-.*/\1.\2/')
 
 echo "CURRENT_VERSION:${CURRENT_VERSION}"
