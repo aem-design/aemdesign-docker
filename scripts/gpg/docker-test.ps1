e3d02dc4330d772d903f953c23c64dfe3f794028
wildone <max.barrass@gmail.com>
2021-08-10 15:40:46 +1000

    add extra debug line.

--- scripts/gpg/docker-test.ps1
+++ scripts/gpg/docker-test.ps1
@@ -7,6 +7,7 @@ Write-Output "Test Ubuntu"
 Write-Output ""
 Write-Output ""
 
+#docker run -it --rm -v ${PWD}:/build/source -v ${HOME}/.m2:/build/.m2 -e GPG_SECRET_KEYS=${GPG_SECRET_KEYS} -e GPG_OWNERTRUST -e GPG_PASSPHRASE -e GPG_PUBID -e GPG_PUBID_KEYGRIP --net=host ubuntu /bin/bash --login
 docker run -it --rm -v ${PWD}:/build/source -v ${HOME}/.m2:/build/.m2 -e GPG_SECRET_KEYS=${GPG_SECRET_KEYS} -e GPG_OWNERTRUST -e GPG_PASSPHRASE -e GPG_PUBID -e GPG_PUBID_KEYGRIP --net=host ubuntu /bin/bash --login -c "apt-get update && apt-get install -y gpg && bash /build/source/setup-gpg.sh"
 
 Write-Output ""
