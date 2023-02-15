8025a2786ff5a5d0fec01f9cd736e3af16b23f91
wildone <max.barrass@gmail.com>
2023-02-15 13:35:50 +1100

    migrate vars output.

--- scripts/github/github_get_version.sh
+++ scripts/github/github_get_version.sh
@@ -20,31 +20,33 @@ if [[ "$GITHUB_EVENT_NAME" == "pull_request" ]]; then
 fi
 echo "GIT_BRANCH:${GIT_BRANCH}"
 echo "GIT_BRANCH=${GIT_BRANCH}" >> $GITHUB_ENV
-echo ::set-output name=GIT_BRANCH::${GIT_BRANCH}
-
+echo "GIT_BRANCH=${GIT_BRANCH}" >> $GITHUB_OUTPUT
 
 
 echo "CURRENT_VERSION:${CURRENT_VERSION}"
 declare -a CURRENT_VERSION_ARRAY="(${CURRENT_VERSION//./ })";
 export SEMVER_MAJOR=${CURRENT_VERSION_ARRAY[0]};
 echo "SEMVER_MAJOR=${SEMVER_MAJOR}" >> $GITHUB_ENV
-echo ::set-output name=SEMVER_MAJOR::${SEMVER_MAJOR}
+echo "SEMVER_MAJOR=${SEMVER_MAJOR}" >> $GITHUB_OUTPUT
+
 export SEMVER_MINOR=${CURRENT_VERSION_ARRAY[1]};
 echo "SEMVER_MINOR=${SEMVER_MINOR}" >> $GITHUB_ENV
-echo ::set-output name=SEMVER_MINOR::${SEMVER_MINOR}
+echo "SEMVER_MINOR=${SEMVER_MINOR}" >> $GITHUB_OUTPUT
+
 export SEMVER_PATCH=${CURRENT_VERSION_ARRAY[2]};
 echo "SEMVER_PATCH=${SEMVER_PATCH}" >> $GITHUB_ENV
-echo ::set-output name=SEMVER_PATCH::${SEMVER_PATCH}
+echo "SEMVER_PATCH=${SEMVER_PATCH}" >> $GITHUB_OUTPUT
+
 export SEMVER_BUILD=${CURRENT_VERSION_ARRAY[-1]}
 echo "SEMVER_BUILD=${SEMVER_BUILD}" >> $GITHUB_ENV
-echo ::set-output name=SEMVER_BUILD::${SEMVER_BUILD}
+echo "SEMVER_BUILD=${SEMVER_BUILD}" >> $GITHUB_OUTPUT
 
 # if tag already has MAJOR.MINOR.PATCH add git log commit count to patch
 if [[ "${#CURRENT_VERSION_ARRAY[@]}" == "4" ]]; then
     echo "ADD PATCH TO BUILD VERSION"
     export SEMVER_BUILD=$(( ${SEMVER_PATCH} + ${SEMVER_BUILD} ))
   echo "SEMVER_BUILD=${SEMVER_BUILD}" >> $GITHUB_ENV
-  echo ::set-output name=SEMVER_BUILD::${SEMVER_BUILD}
+  echo "SEMVER_BUILD=${SEMVER_BUILD}" >> $GITHUB_OUTPUT
 fi
 
 echo "SEMVER_MAJOR:${SEMVER_MAJOR}"
@@ -59,7 +61,7 @@ fi
 
 export SEMVER=${SEMVER_MAJOR}.${SEMVER_MINOR}.${SEMVER_BUILD}
 echo "SEMVER=${SEMVER}" >> $GITHUB_ENV
-echo ::set-output name=SEMVER::${SEMVER}
+echo "SEMVER=${SEMVER}" >> $GITHUB_OUTPUT
 echo "SEMVER:${SEMVER}"
 if [[ ${SEMVER_MAJOR} == "" ]];then
     echo PLEASE ADD TAG TO YOUR BRANCH
@@ -67,12 +69,13 @@ if [[ ${SEMVER_MAJOR} == "" ]];then
 fi
 export GITHUB_TAG=${SEMVER}
 echo "GITHUB_TAG=${GITHUB_TAG}" >> $GITHUB_ENV
-echo ::set-output name=GITHUB_TAG::${GITHUB_TAG}
+echo "GITHUB_TAG=${GITHUB_TAG}" >> $GITHUB_OUTPUT
+
 echo "GITHUB_TAG:${GITHUB_TAG}"
 export GIT_RELEASE_NOTES="$(git log $(git describe --tags --abbrev=0 --always)..HEAD --pretty=format:"%h - %s (%an)<br>")"
 #echo "GIT_RELEASE_NOTES=${GIT_RELEASE_NOTES}" >> $GITHUB_ENV
-echo ::set-output name=GIT_RELEASE_NOTES::${GIT_RELEASE_NOTES}
+echo "GIT_RELEASE_NOTES=${GIT_RELEASE_NOTES}" >> $GITHUB_OUTPUT
 
 #set CURRENT_VERSION to semver
 echo "CURRENT_VERSION=${GITHUB_TAG}" >> $GITHUB_ENV
-echo ::set-output name=CURRENT_VERSION::${GITHUB_TAG}
+echo "CURRENT_VERSION=${GITHUB_TAG}" >> $GITHUB_OUTPUT
